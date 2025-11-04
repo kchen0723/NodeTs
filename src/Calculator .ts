2de14eaa818a6d4d003154b1b3950b64398144c0
kissinger <kchen0723@gmail.com>
2025-11-03 19:20:28 -0800

    add Calculator .ts

--- src/Calculator .ts
+++ src/Calculator .ts
@@ -2,4 +2,69 @@ export class Calculator {
     add(a: number, b: number): number {
         return a + b;
     }
+
+    public Calculate(str: string): number{
+        if(!str){
+            return 0;
+        }
+        let strArray = Array.from(str);
+        let result = this.CalculateHelp(strArray);
+        return result;
+    }
+
+    private CalculateHelp(strArray: string[]): number{
+        let sign = "+";
+        var tokens = [];
+        var stack: number[] = [];
+        
+        while(strArray.length > 0){
+            let token = strArray[0];
+            strArray.shift();
+            
+            if((token >= "0" && token <="9") || token === "."){
+                tokens.push(token);
+            } 
+            if(token === "+" || token === "-" || token === "*" || token === "/" ||
+                token === "(" || token === ")" || strArray.length === 0) {
+                let num = 0;
+                if(tokens.length > 0){
+                    num = Number(tokens.join(""));
+                    tokens.length = 0;
+                }
+                if(token === "("){
+                    num = this.CalculateHelp(strArray);
+                }
+                switch(sign){
+                    case "+":
+                        stack.push(num);
+                        break;
+                    case "-":
+                        stack.push(0 - num);
+                        break;
+                    case "*":
+                        var last = stack.pop();
+                        if(last){
+                            stack.push(last * num);
+                        }
+                        break;
+                    case "/":
+                        var last = stack.pop();
+                        if(last){
+                            stack.push(last / num);
+                        }
+                        break;
+                }
+                if(token === ")"){
+                    break;
+                }
+                sign = token;
+            }
+        }
+
+        let result = 0;
+        for(let i = 0; i < stack.length; i++){
+            result += stack[i];
+        }
+        return result;
+    }
 }
--- src/tests/Calculator.test.ts
+++ src/tests/Calculator.test.ts
@@ -11,4 +11,15 @@ describe("calculator", () => {
         var actual = cal.add(3, 5);
         expect(actual).toBe(8);
     });
+
+    test("Calculate a string", () => {
+        var actual = cal.Calculate("9-10+11");
+        expect(actual).toBe(10);
+
+        actual = cal.Calculate("1+((9-3)*4-10)/(6-4)+1");
+        expect(actual).toBe(9);
+
+        actual = cal.Calculate("1+((3*((9-3)*4-10)/(6-4)+1)+2)/3+5");
+        expect(actual).toBe(14);
+    });
 });
